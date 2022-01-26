a3544f3dd2928adaf8100ad3df9b7290b561dfb4
Bohdan Suprun <bohdan-suprun@outlook.com>
2022-01-26 21:47:00 +0200

    git token env

--- app/Http/Controllers/Api/GitController.php
+++ app/Http/Controllers/Api/GitController.php
@@ -10,7 +10,7 @@ class GitController extends Controller
 {
     public static function getLastVersionTag()
     {
-        $response = Http::withToken('glpat-9t25RFfHWk8dZq3LQs3A')->get('http://gitlab.com/api/v4/projects/33163427/repository/tags');
+        $response = Http::withToken(config('app.gitToken'))->get('http://gitlab.com/api/v4/projects/33163427/repository/tags');
         $data = $response->json();
         $data = $data[0]['name'];
         return $data;
