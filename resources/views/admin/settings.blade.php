6dd4647171a80f3781f4f66cb087d8e0edc417bf
Bohdan Suprun <bohdan@bsc.cv.ua>
2022-02-11 11:12:50 +0200

    Enctype mfd. Setting forms

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-6">
             <div class="tile">
                 <h3 class="tile-title">Соціальні мережі</h3>
-                <form action="#">
+                <form action="#" enctype="multipart/form-data">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group">
@@ -38,109 +38,24 @@
         </div>
         <div class="col-md-6">
             <div class="tile">
-                <h3 class="tile-title">Animated Toggle Button</h3>
+                <h3 class="tile-title">Логотипи</h3>
+                <form action="#" enctype="multipart/form-data">
                 <div class="row">
-                    <div class="col-md-6">
-                        <p><b>Toggle Button</b></p>
-                        <div class="toggle">
-                            <label>
-                                <input type="checkbox"><span class="button-indecator"></span>
-                            </label>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="exampleInputFile">Favicon</label>
+                            <input class="form-control-file" id="exampleInputFile" type="file" aria-describedby="fileHelp"><small class="form-text text-muted" id="fileHelp">Це зображення буде виводитись у вкладці повинно мати розмір 16х16 або 40х40 або 64х64</small>
                         </div>
-                        <div class="toggle lg">
-                            <label>
-                                <input type="checkbox"><span class="button-indecator"></span>
-                            </label>
+                        <div class="form-group">
+                            <label for="exampleInputFile">Головне лого</label>
+                            <input class="form-control-file" id="exampleInputFile" type="file" aria-describedby="fileHelp"><small class="form-text text-muted" id="fileHelp">Це зображення буде виводитись по центру зверху</small>
                         </div>
-                        <h5>Disabled state</h5>
-                        <div class="toggle">
-                            <label>
-                                <input type="checkbox" disabled=""><span class="button-indecator"></span>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <p><b>Fliping Toggle Button</b></p>
-                        <div class="toggle-flip">
-                            <label>
-                                <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
-                            </label>
-                        </div>
-                        <h5>Disabled state</h5>
-                        <div class="toggle-flip">
-                            <label>
-                                <input type="checkbox" disabled=""><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
-                            </label>
+                        <div class="tile-footer">
+                            <button class="btn btn-primary" type="submit">Зберегти</button>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="clearfix"></div>
-        <div class="col-md-6">
-            <div class="tile">
-                <div class="tile-title-w-btn">
-                    <h3 class="title">Select2</h3>
-                    <p><a class="btn btn-primary icon-btn" href="https://select2.github.io/examples.html" target="_blank"><i class="fa fa-file"></i>Docs</a></p>
-                </div>
-                <div class="tile-body">
-                    <p>This plugin can be used to convert select element into advanced componant.</p>
-                    <h4>Demo</h4>
-                    <select class="form-control" id="demoSelect" multiple="">
-                        <optgroup label="Select Cities">
-                            <option>Ahmedabad</option>
-                            <option>Surat</option>
-                            <option>Vadodara</option>
-                            <option>Rajkot</option>
-                            <option>Bhavnagar</option>
-                            <option>Jamnagar</option>
-                            <option>Gandhinagar</option>
-                            <option>Nadiad</option>
-                            <option>Morvi</option>
-                            <option>Surendranagar</option>
-                            <option>Junagadh</option>
-                            <option>Gandhidham</option>
-                            <option>Veraval</option>
-                            <option>Ghatlodiya</option>
-                            <option>Bharuch</option>
-                            <option>Anand</option>
-                            <option>Porbandar</option>
-                            <option>Godhra</option>
-                            <option>Navsari</option>
-                            <option>Dahod</option>
-                            <option>Botad</option>
-                            <option>Kapadwanj</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="tile">
-                <div class="tile-title-w-btn">
-                    <h3 class="title">Date Picker</h3>
-                    <p><a class="btn btn-primary icon-btn" href="http://bootstrap-datepicker.readthedocs.org/en/stable/options.html" target="_blank"><i class="fa fa-file"></i>Docs</a></p>
-                </div>
-                <div class="tile-body">
-                    <p>This plugin can be used to let the user select the date in a convinient way.</p>
-                    <h4>Demo</h4>
-                    <input class="form-control" id="demoDate" type="text" placeholder="Select Date">
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="tile">
-                <div class="tile-title-w-btn">
-                    <h3 class="title">Dropzone</h3>
-                    <p><a class="btn btn-primary icon-btn" href="https://gitlab.com/meno/dropzone" target="_blank"><i class="fa fa-file"></i>Docs</a></p>
-                </div>
-                <div class="tile-body">
-                    <p>This plugin can be used to let the user Drag and Drop files for upload in a easy way.</p>
-                    <h4>Demo</h4>
-                    <form class="text-center dropzone" method="POST" enctype="multipart/form-data" action="/file-upload">
-                        <div class="dz-message">Drop files here or click to upload<br><small class="text-info">(This is just a dropzone demo. Selected files are not actually uploaded.)</small></div>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
     </div>
