087d72c192420835bc0cefd99c516afb66b4c0fa
Bohdan Suprun <bohdan@bsc.cv.ua>
2022-02-13 13:10:40 +0200

    Menu reliase

--- resources/views/template/partials/nav.blade.php
+++ resources/views/template/partials/nav.blade.php
@@ -1,5 +1,7 @@
 <?php
+
 use App\Nav;
+
 $datasets = Nav::getNav();
 ?>
 <nav class="nav justify-content-center">
@@ -8,42 +10,20 @@ $datasets = Nav::getNav();
             <a class="nav-link" href="{{ $nav->uri }}">{{ $nav->value }}</a>
         @endforeach
     @else
-        <a class="nav-link active" href="#">Про школу</a>
-        <a class="nav-link" href="#">Вступ</a>
-        <a class="nav-link" href="#">Для батьків</a>
         <a class="nav-link" href="{{ url('/news') }}">Новини</a>
-
-{{--
-            <pre>
-                <? print_r($datasets) ?>
-            </pre>
---}}
-
-            <?php
-            foreach ($datasets as $k => $v) {
-                echo "<ul style='font-weight: bold'>";
-                echo $k;
-                foreach ($v as $item) {
-                    echo "<li style='font-weight: lighter'>";
-                    echo $item['value'] . " | " .$item['uri'];
-                    echo "</li>";
-                }
-                echo "</ul>";
-            }
-
-            ?>
-            @foreach($datasets as $k => $v)
+        @foreach($datasets as $k => $v)
             <div class="btn-group">
-            <button type="button" class="btn nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {{ $k }}
-            </button>
-            <div class="dropdown-menu">
-                @foreach($v as $item)
-                <a class="dropdown-item" href="{{ $item['uri'] }}">{{ $item['value'] }}</a>
-                @endforeach
+                <button type="button" class="btn nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                    {{ $k }}
+                </button>
+                <div class="dropdown-menu">
+                    @foreach($v as $item)
+                        <a class="dropdown-item" href="{{ $item['uri'] }}">{{ $item['value'] }}</a>
+                    @endforeach
+                </div>
             </div>
-            </div>
-            @endforeach
+        @endforeach
     @endif
     @if(config('app.login') == true)
         @if(!Auth::check())
