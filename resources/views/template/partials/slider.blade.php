0d60810a36cd8c5a5ec8e202d6c12ce1e0b8f509
Bohdan Suprun <bohdan.suprun@sapient.pro>
2023-10-19 22:20:58 +0000

    Fix slider navigation

--- resources/views/template/partials/slider.blade.php
+++ resources/views/template/partials/slider.blade.php
@@ -1,11 +1,13 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        @php($i = 0)
-        @foreach($sliders as $key => $slide)
-            <li data-target="#carouselExampleIndicators" data-slide-to="{{ $i }}" class="{{ $key == 0 ? 'active' : '' }}"></li>
-            @php($i++)
-        @endforeach
-    </ol>
+    @if(count($sliders) > 1)
+        <ol class="carousel-indicators">
+            @php($i = 0)
+            @foreach($sliders as $key => $slide)
+                <li data-target="#carouselExampleIndicators" data-slide-to="{{ $i }}" class="{{ $key == 0 ? 'active' : '' }}"></li>
+                @php($i++)
+            @endforeach
+        </ol>
+    @endif
     <div class="carousel-inner">
         @foreach($sliders as $slide)
             <div class="carousel-item">
@@ -16,23 +18,21 @@
                     @else
                         <a href="http://{{ $slide->url ?? "#" }}"><span class="slider-description">{{ $slide->preview_text }}</span></a>
                     @endif
-
                 @endif
             </div>
         @endforeach
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+    @if(count($sliders) > 1)
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    @endif
 </div>
 <script>
     $('.carousel-item').first().addClass('active')
 </script>
-
-
-
