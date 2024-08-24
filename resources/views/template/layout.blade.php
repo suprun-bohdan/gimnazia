0ad4dff635823309301b13e05d94fad78429cdb7
Bohdan Suprun <bohdan-suprun@outlook.com>
2024-08-24 01:25:12 +0000

    footer fix

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -87,7 +87,7 @@
 </div>
 <!--END CONTAINER-->
 
-<footer class="container">
+<footer class="container-fluid">
     <div class="row text-center">
         <div class="col-4">
             <ul class="list-unstyled">
