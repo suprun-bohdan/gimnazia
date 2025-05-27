504c0ec6ec1887579bcef3dd0f5f5c5818e5e4ea
Bohdan Suprun <bohdan-suprun@outlook.com>
2025-05-27 10:17:07 +0000

    Update

--- docker/start-php-fpm.sh
+++ docker/start-php-fpm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec /usr/local/sbin/php-fpm --nodaemonize --fpm-config /usr/local/etc/php-fpm.conf >> /var/log/php-fpm.log 2>&1
+exec /usr/local/sbin/php-fpm --nodaemonize --fpm-config /usr/local/etc/php-fpm.conf --error-log /var/log/php-fpm.log
